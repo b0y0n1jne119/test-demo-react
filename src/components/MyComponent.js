9f52efd1a7a5715f6b1d0025e1ae904736b7d47f
Minh Duc <nguyenminhduc119@gmail.com>
2023-01-16 11:01:51 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <div>{Math.random()}</div>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
