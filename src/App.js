93eadaf178cec6b95afb4b27024fb77b28245f7d
Minh Duc <nguyenminhduc119@gmail.com>
2023-01-18 23:41:03 +0700

    video 17

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-
 import MyComponent from './components/MyComponent';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component {
 
-  return (
-    <div>
-      Minh Duc
-      <MyComponent>
+  render() {
+    return (
+      <div>
+        Minh Duc
+        < MyComponent >
 
-      </MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Steins;Gate Okabe
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+        </MyComponent >
+      </div>
+    );
+  }
 }
+
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div>
+//       Minh Duc
+//       <MyComponent>
+
+//       </MyComponent>
+//     </div>
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Steins;Gate Okabe
+//     //     </p>
+//     //     <div>Count = {count}</div>
+//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,11 +3,18 @@ import React from 'react';
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Minh Duc',
+        address: 'Ha Noi',
+        age: 26
+    }
+
+    //JSX
     render() {
         return (
 
             <div>
-                <div>{Math.random()}</div>
+                My name is {this.state.name} and i'm {this.state.age} years old
             </div>
         );
     }
