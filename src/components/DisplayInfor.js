f13d45627c5f777d1511b070a42709f1b3421136
Minh Duc <nguyenminhduc119@gmail.com>
2023-02-07 18:11:10 +0700

    video 38

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,38 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DisplayInfor.scss';
-// import screen from './../logo.svg';
+import screen from './../logo.svg';
 
-// class DisplayInfor extends React.Component {
-
-
-//     render() {
-//         console.log(">>> Call me render")
-//         //destructuring array / object
-//         const { listUsers } = this.props; //object
-//         //props => viết tắt của properties
-//         return (
-//             <div className="display-infor-container">
-
-//                 {true &&
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"} >
-//                                     <div style={{ color: 'blue' }}>My name's is {user.name}</div>
-//                                     <div>My age's is {user.age}</div>
-//                                     <hr />
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete </button>
-//                                     </div>
-//                                 </div>
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props; //object
@@ -42,6 +11,14 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);
     }
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert("You gust")
+            }
+        }, [listUsers]
+    );
 
     return (
         <div className="display-infor-container">
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,55 +3,6 @@ import DisplayInfor from './DisplayInfor';
 import AddUserInfor from './AddUserInfor';
 
 
-// class MyComponent extends React.Component {
-
-//     state = {
-//         listUsers: [
-//             { id: 1, name: "Minh Duc", age: "11" },
-//             { id: 2, name: "The Dung", age: "24" },
-//             { id: 3, name: "Duc Tu", age: "25" },
-//         ]
-//     }
-
-//     handleAddNewUser = (userObj) => {
-//         this.setState({
-//             listUsers: [userObj, ...this.state.listUsers]
-//         })
-//     }
-
-//     handleDeleteUser = (userID) => {
-//         let listUsersClone = [...this.state.listUsers];
-//         listUsersClone = listUsersClone.filter(item => item.id !== userID);
-//         this.setState({
-//             listUsers: listUsersClone
-//         });
-//     }
-
-//     //JSX
-//     render() {
-//         //DRY: don't repeat yourself
-//         return (
-
-//             <>
-//                 <div className="a">
-//                     <AddUserInfor
-//                         handleAddNewUser={this.handleAddNewUser}
-//                     />
-//                     <br /><br />
-//                     <DisplayInfor
-//                         listUsers={this.state.listUsers}
-//                         handleDeleteUser={this.handleDeleteUser}
-//                     />
-//                 </div>
-
-//                 <div className='b'>
-
-//                 </div>
-//             </>
-//         );
-//     }
-// }
-
 const MyComponent = (props) => {
 
     const [listUsers, setlistUsers] = useState(
