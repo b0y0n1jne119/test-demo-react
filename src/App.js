2ce9fcee2caef34ca72891db23d26be38b445464
Minh Duc <nguyenminhduc119@gmail.com>
2023-02-08 10:20:51 +0700

    video 46

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-import logo from './logo.svg';
 import './App.scss';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
 
-class App extends React.Component {
 
-  render() {
-    return (
-      <div className="app-container">
-        < MyComponent />
-      </div>
-    );
-  }
+const App = () => {
+
+  return (
+    <div className="app-container">
+      Minh Duc
+    </div>
+  );
 }
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       Minh Duc
-//       <MyComponent>
-
-//       </MyComponent>
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Steins;Gate Okabe
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// }
 
 export default App;
