6cc98b209b299fabc85443cc299ecc2d387c19ed
Minh Duc <nguyenminhduc119@gmail.com>
2023-02-08 22:09:20 +0700

    video 51

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,14 @@
+import videoHomePage from '../../assets/video-homepage.mp4';
+
 export default function HomePage(props) {
     return (
-        <div className="homepage-container">Home</div>
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source
+                    data-testid="currentDefaultVideo"
+                    src={videoHomePage}
+                    type="video/mp4" />
+            </video>
+        </div>
     )
 }
